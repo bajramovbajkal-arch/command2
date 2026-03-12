06b15e8107187f65212d6c5d0e2fb9ee7bc18cb5
sultanbekb <sultanbekbayjanov333@gmail.com>
2026-03-12 16:06:01 +0300

    asd

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+button_1 = KeyboardButton(text="about")
+button_2 = KeyboardButton(text="contact")
+
+menu_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [button_1, button_2],
+
+    ],
+    resize_keyboard=True,
+)
 import os
 from dotenv import load_dotenv
 load_dotenv()
